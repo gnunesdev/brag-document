0a2e1275dfae8da150460c1b18bb15d9f197d19f
gnunesdev <gnunes.programmer@gmail.com>
2023-09-18 22:13:21 -0300

    feat(shared/components): adds shadcn initial setup

--- modules/shared/components/src/index.ts
+++ modules/shared/components/src/index.ts
@@ -1,3 +1,1 @@
-// Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
-
-export * from './lib/components';
+export * from './components/ui/button';
