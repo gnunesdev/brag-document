d5f6bd1148983b1d69ce6cf35613aa1fc57a73f8
gnunesdev <gnunes.programmer@gmail.com>
2023-09-18 22:13:21 -0300

    feat(shared/components): adds shadcn initial setup

--- apps/brag-document/app/page.tsx
+++ apps/brag-document/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@brag-document/components';
+
 export default async function Index() {
-  return <div className="bg-indigo-950">hello world!</div>;
+  return (
+    <div className="bg-indigo-950 flex flex-col w-32">
+      hello world!
+      <Button>say hi</Button>
+    </div>
+  );
 }
